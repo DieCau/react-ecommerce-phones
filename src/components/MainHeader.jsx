2d47c3ead533777aec0ae7c64a42576adcf150b7
Diego Caucota <diegocaucota@gmail.com>
2024-07-13 12:09:22 -0300

    fix: .prettierrc

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -4,19 +4,19 @@ import "@/../src/index.css";
 
 const MainHeader = () => {
   return (
-    <header>
+    <header className="flex container bg-slate-400 mx-auto px-4">
       <button className="md:hidden">
         <span className="material-symbols-outlined">Menu</span>
       </button>
-      <img className="h-20" src={logoEmpresa} alt="logo" />
-      <nav className="hidden md:block">
+      <img className="h-20 mr-auto md:mr-auto" src={logoEmpresa} alt="logo" />
+      <nav className="hidden md:block md:mr-auto">
         <a href="#">Colecciones</a>
         <a href="#">Hombres</a>
         <a href="#">Mujeres</a>
         <a href="#">Nosotros</a>
         <a href="#">Contacto</a>
       </nav>
-      <div>
+      <div className="flex gap-5">
         <button>
           <span className="material-symbols-outlined">shopping_cart</span>
         </button>

219a02bc827e0040ddff7254c0ccd142132bf522
Diego Caucota <diegocaucota@gmail.com>
2024-07-15 20:10:13 -0300

    fix: nav

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -4,13 +4,21 @@ import MenuIcon from "./MenuIcon";
 import CartIcon from "./CartIcon";
 
 const MainHeader = () => {
+  const handleOpenMenu = () => {
+    console.log("click");
+  };
+
   return (
-    <header className="container mx-auto flex bg-slate-400 px-4">
-      <button className="md:hidden">
+    <header className="container mx-auto flex items-center gap-8 bg-gray-200 px-4 py-8">
+      <button className="md:hidden" onClick={handleOpenMenu}>
         <MenuIcon />
       </button>
-      <img className="mr-auto md:mr-0" src={logoEmpresa} alt="logo" />
-      <nav className="hidden md:mr-auto md:block">
+      <img
+        className="mb-1 mr-auto h-5 md:mr-0"
+        src={logoEmpresa}
+        alt="logo empresa"
+      />
+      <nav className="w-4/5 font-extrabold h-full p-8 gap-y-5 absolute top-0 left-0 bg-gray-400 flex flex-col md:mr-auto md:flex md:flex-row md:gap-4">
         <a href="#">Colecciones</a>
         <a href="#">Hombres</a>
         <a href="#">Mujeres</a>
@@ -21,11 +29,8 @@ const MainHeader = () => {
         <button>
           <CartIcon />
         </button>
+        <img className="w-10" src={avatar} alt="avatar" />
       </div>
-      <img
-        src={avatar}
-        alt="avatar"
-      />
     </header>
   );
 };
